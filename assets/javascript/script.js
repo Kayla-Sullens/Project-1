f2379f52e560f2321cb6a3bc31386ea269d1aff5
Jaime <quezadajaime@ymail.com>
2023-08-10 21:36:47 -0700

    Search function logging

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,42 @@
-var input = document.getElementById("input-box");
-var button = document.getElementById("submit-button");
-var displayContainer = document.getElementById("display-container");
-var resultsContainer = document.querySelector(".list");
-var apiKey = "62950945477f52a6587962327ba60ae0"
+var input = document.getElementById("character-input");
+var searchBtn = document.getElementById("searchBtn");
+var trailersBtn = document.getElementById("trailersBtn");
+var bioBtn = document.getElementById("bioBtn");
+var apiKey = "62950945477f52a6587962327ba60ae0";
+var privateKey = "376d22d9276bc21ecc8dea681b71371870cdfab2"
 
-button.addEventListener("click", function() {
+
+
+function getCharacter(character) {
+    var ts = new Date().getTime();
+    var hash = md5(ts + privateKey + apiKey);
+    console.log(hash);
+
+    var url = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&name=${character}&apikey=${apiKey}&hash=${hash}`;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+//var date = new Date();
+//console.log(date.getTime());
+
+//const [timeStamp, apiKey, hashVal] = [ts, apiKey, hashVal];
+
+searchBtn.addEventListener("click", function () {
     if (input.value.length < 1) {
         alert("Input value cannot be left blank");
+    } else  {
+        getCharacter(input.value);
     }
-    displayContainer.innerHTML = "";
-        var url = 
-})
+
+});
+
+
+
+
+
