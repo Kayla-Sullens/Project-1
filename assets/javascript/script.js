e7c9b455bcfdfb8b9b35b4c0af1dc54e65be7774
IgnacioG08 <134964013+IgnacioG08@users.noreply.github.com>
2023-08-14 20:01:58 -0700

    fixed variable

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,8 +3,8 @@ var searchBtn = document.getElementById("searchBtn");
 var trailersBtn = document.getElementById("trailersBtn");
 var bioBtn = document.getElementById("bioBtn");
 var apiKey = "62950945477f52a6587962327ba60ae0";
-var privateKey = "376d22d9276bc21ecc8dea681b71371870cdfab2"
-
+var privateKey = "376d22d9276bc21ecc8dea681b71371870cdfab2";
+var character = document.getElementsByClassName("character");
 
 
 function getCharacter(character) {
@@ -14,38 +14,33 @@ function getCharacter(character) {
 
     var url = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&name=${character}&apikey=${apiKey}&hash=${hash}`;
     fetch(url)
-    var response = fetch(url);
-    var jsonData = response.json();
-    jsonData.data["results"].forEach(element)() 
-        character.innerHTML = `
-        <div class ="results-container">
-            <div class ="character-name"> ${element.name} </div>
-            <div class ="character-image">
-                <img src ="${element.thumbnail["path"] + "." + element.thumbnail["extension"]}">
-            </div>
-            <div class ="character-bio"> ${element.description} </div>
-        </div>`;
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayCharacter(data);}
+            );
 }
 
-//var date = new Date();
-//console.log(date.getTime());
+function displayCharacter(element) { 
 
-//const [timeStamp, apiKey, hashVal] = [ts, apiKey, hashVal];
+    character.textContent= `
+<div class ="results-container">
+    <div class ="character-name"> ${element.name} </div>
+    <div class ="character-image">
+        <img src ="${element.thumbnail}">
+    </div>
+    <div class ="character-bio"> ${element.description} </div>
+</div>`;
+}
 
 searchBtn.addEventListener("click", function () {
     if (input.value.length < 1) {
         alert("Input value cannot be left blank");
-     } else  {
-         getCharacter(input.value);
-         getCharacter.innerHTML = "";
-   
-     }
+    } else {
+        getCharacter(input.value);
+        getCharacter.innerHTML = "";
+
+    }
 
 });
-
-
-
-
-
-
-
